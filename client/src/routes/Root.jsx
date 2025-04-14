dac632c32e09d5ac04e236926f64989f64c86775
arjun-saseendran <arjunsaseendran@icloud.com>
2025-04-14 10:43:40 +0530

    format code v0

--- client/src/routes/Root.jsx
+++ client/src/routes/Root.jsx
@@ -3,10 +3,8 @@ import { Outlet } from "react-router-dom";
 import { Footer } from "../components/Footer/Footer";
 import { useAuth } from "../hooks/useAuth";
 import { useNavigate } from "react-router-dom";
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import { useEffect } from "react";
-import axios from "axios";
-import { cartProducts } from "../features/cart/cartSlice";
 
 export const Root = () => {
   useAuth();
@@ -22,7 +20,6 @@ export const Root = () => {
   }, [user, loading, navigate]);
 
   useEffect(() => {
-    console.log(user?.mobile);
     const handleUnload = () => {
       if (!user || !user.mobile || cart?.products?.length === 0) return;
 
@@ -31,7 +28,7 @@ export const Root = () => {
         JSON.stringify({
           phone: user?.mobile,
           cart: cart,
-        }),
+        })
       );
     };
 
